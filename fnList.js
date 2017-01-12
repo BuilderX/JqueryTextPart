d41a8dede67a4e75570f278f37fe8b6caf8c7cb7
Raymond Gonzalez <raymondgon7@gmail.com>
2017-01-12 11:10:13 -0500

    Update fnList.js

--- fnList.js
+++ fnList.js
@@ -1,10 +1,19 @@
  (function( $ ) {
     $.fn.partOfWord = function(str,end){
-     if(typeof str && end === 'number'){
-   	    if(str || end < 1){return null}
-        var p = this.clone().children().remove().end().text().substr(str,end);
-   	    this.text(p);
-   	  }
-    }
+       if(typeof str && end === 'number'){
+   	     if(str || end < 1){return null}
+           var p = this.clone().children().remove().end().text().substr(str,end);
+   	       this.text(p);
+   	    }
+     }
  
+    $.fn.textPortion = function(str,end){
+       if(typeof str && end === 'number'){
+   	     if(str || end < 1){return null}
+            var txt = this.clone().children().remove().end().text().substr(str,end);
+              return txt;
+          }
+       }
+    
+    
 }( jQuery ));
