a3c7840f8a60caf77380028ee1a59f61b526c656
Raymond Gonzalez <raymondgon7@gmail.com>
2017-01-12 10:55:56 -0500

    Create fnList.js

---
+++ fnList.js
@@ -0,0 +1,17 @@
+ (function( $ ) {
+
+    $.fn.partsOfWord = function(str,end){
+   	    if(str || end < 1){return null}
+        var p = this.clone().children().remove().end()
+   	    .text().substr(str,end);
+   	    p.text(p);
+   	  }
+      
+      
+      
+      
+      
+      
+      
+ 
+}( jQuery ));
