51282d95ee900c7a0cfb15342e2a520cc62a47bb
Raymond Gonzalez <raymondgon7@gmail.com>
2017-01-12 11:04:55 -0500

    Update fnList.js

--- fnList.js
+++ fnList.js
@@ -1,10 +1,10 @@
  (function( $ ) {
-
     $.fn.partOfWord = function(str,end){
+     if(typeof str && end === 'number'){
    	    if(str || end < 1){return null}
         var p = this.clone().children().remove().end().text().substr(str,end);
-   	    p.text(p);
+   	    this.text(p);
    	  }
-      
+    }
  
 }( jQuery ));
