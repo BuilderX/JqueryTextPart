48226287e1faac696686d7cd60d0c3c1a8edd256
Raymond Gonzalez <raymondgon7@gmail.com>
2017-01-12 10:56:25 -0500

    Update fnList.js

--- fnList.js
+++ fnList.js
@@ -2,16 +2,9 @@
 
     $.fn.partsOfWord = function(str,end){
    	    if(str || end < 1){return null}
-        var p = this.clone().children().remove().end()
-   	    .text().substr(str,end);
+        var p = this.clone().children().remove().end().text().substr(str,end);
    	    p.text(p);
    	  }
-      
-      
-      
-      
-      
-      
       
  
 }( jQuery ));
